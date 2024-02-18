1ddf1e8bac65d04604aabd5259dcbf9c083fcc66
Pawel Fajfer <fajfer.pawel@gmail.com>
2024-02-18 20:21:00 +0100

    Added changing mob direction on wall hit

--- pig_mob.gd
+++ pig_mob.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 30.0
+const SPEED = 300.0
 var direction = 1;
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,6 +13,9 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if is_on_wall():
+		direction = direction *-1
 	
 	if not _ray_cast_right.is_colliding():
 		direction = -1
