afa64d4c1f9b64bf57ec1fdd023b0701d1459096
Pawel Fajfer <fajfer.pawel@gmail.com>
2024-02-20 14:08:09 +0100

    Added run and attack animations to pig mob

--- pig_mob.gd
+++ pig_mob.gd
@@ -10,8 +10,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var _ray_cast_right = $RayCast2DRight
 @onready var _ray_cast_left = $RayCast2DLeft
 @onready var animation_player = $AnimationPlayer
-@onready var idle_sprite = $Idle
-@onready var attack_sprite = $Attack
+@onready var animations = $Animations
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -27,13 +26,17 @@ func _physics_process(delta):
 	
 	if not _ray_cast_left.is_colliding():
 		direction = 1
-	
+
+		
 	if direction == -1:
-		idle_sprite.flip_h = true
-		idle_sprite.offset.x = 5
+		animations.flip_h = false
+		animations.offset.x = 5
 	else:
-		idle_sprite.flip_h = false
+		animations.flip_h = true
 	
 	velocity.x = direction * SPEED
+	
+	if velocity.x != 0 && animation_player.current_animation != "Attack":
+		animation_player.play("Run")
 	
 	move_and_slide()
