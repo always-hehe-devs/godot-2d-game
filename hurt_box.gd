3d73fd25b670a4ea175fc94ad1657bf45be69046
Pawel Fajfer <fajfer.pawel@gmail.com>
2024-02-23 12:33:09 +0100

    Moved to animation tree for player

--- hurt_box.gd
+++ hurt_box.gd
@@ -9,5 +9,6 @@ func _on_area_entered(area):
 		return
 	
 	if area.name == "HitBox":
+		print("here")
 		if owner.has_method("take_damage"):
 			owner.take_damage(area.damage)
--- player.gd
+++ player.gd
@@ -4,13 +4,17 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 var health = 100.0
+var direction
 
-@onready var _animated_sprite = $AnimatedSprite2D
+@onready var animation_tree = $AnimationTree
+@onready var sprite = $Sprite2D
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta):
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -18,31 +22,38 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	if velocity.y < 0:
-		_animated_sprite.play("Jump")
-	elif velocity.y > 0:
-		_animated_sprite.play("Fall")
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
 		velocity.x = direction * SPEED
-		if velocity.y == 0:
-			_animated_sprite.play("Run")
-		if direction == -1:
-			_animated_sprite.flip_h = true
-			_animated_sprite.offset.x = -14
-		elif direction == 1:
-			_animated_sprite.flip_h = false
-			_animated_sprite.offset.x = 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		_animated_sprite.play("Idle")
-
+	
+	update_animation_parameteres()
+	update_facing_direction()
 	move_and_slide()
 
+func update_animation_parameteres():
+	if velocity.x != 0:
+		print(animation_tree["parameters/conditions/is_moving"])
+		animation_tree.set("parameters/conditions/is_moving", true)
+		animation_tree.set("parameters/conditions/idle", false)
+	else:
+		animation_tree.set("parameters/conditions/is_moving", false)
+		animation_tree.set("parameters/conditions/idle", true)
+	
+	
+func update_facing_direction():
+	if direction < 0: 
+		sprite.flip_h = true
+		sprite.offset.x = -15
+	elif direction > 0:
+		sprite.flip_h = false
+		sprite.offset.x = 0
+		
 func take_damage(damage):
 	health -= damage;
 	%ProgressBar.value = health;
