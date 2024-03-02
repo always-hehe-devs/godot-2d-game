d26977962d5c5e3652507f61568745b1d9b5318e
Pawel Fajfer <fajfer.pawel@gmail.com>
2024-03-02 19:36:11 +0100

    Fixed animations loop

--- hit_state.gd
+++ hit_state.gd
@@ -7,12 +7,11 @@ class_name HitState
 
 func on_enter():
 	playback.travel("Hit")
-	player.move_and_slide()
 
 func state_process(_delta):
 		
 	var direction = move_component.get_movement_direction()
-	
+	print(direction)
 	if player.is_on_floor():
 		if direction != 0:
 			next_state = ground_state
